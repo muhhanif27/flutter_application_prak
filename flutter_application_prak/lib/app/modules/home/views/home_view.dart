86548708d637e1116ea23b7621a5493582a494f1
GILANG140703 <gilanggaming80@gmail.com>
2024-10-01 13:54:48 +0700

    update image

--- flutter_application_prak/lib/app/modules/home/views/home_view.dart
+++ flutter_application_prak/lib/app/modules/home/views/home_view.dart
@@ -9,53 +9,181 @@ class HomeView extends StatefulWidget {
   State<HomeView> createState() => _HomeViewState();
 }
 
-class _HomeViewState extends State<HomeView> {
-  // This is the file that will be used to store the image
-  File? _image;
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  File? _profileImage; // Variabel untuk menyimpan gambar profil
+  final ImagePicker _picker = ImagePicker();
 
-  // This is the image picker
-  final _picker = ImagePicker();
-  // Implementing the image picker
+  // Fungsi untuk mengambil gambar dari galeri atau kamera
   Future<void> _openImagePicker() async {
-    final XFile? pickedImage =
-        await _picker.pickImage(source: ImageSource.gallery);
-    if (pickedImage != null) {
-      setState(() {
-        _image = File(pickedImage.path);
-      });
-    }
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Ambil Foto dari Kamera'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final XFile? pickedImage =
+                      await _picker.pickImage(source: ImageSource.camera);
+                  if (pickedImage != null) {
+                    setState(() {
+                      _profileImage = File(pickedImage.path);
+                    });
+                  }
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: const Text('Pilih dari Galeri'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final XFile? pickedImage =
+                      await _picker.pickImage(source: ImageSource.gallery);
+                  if (pickedImage != null) {
+                    setState(() {
+                      _profileImage = File(pickedImage.path);
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  late TabController _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Please choose your image kntl'),
+      appBar: AppBar(
+        title: const Text('Latihan Dada'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: const [
+            Tab(text: 'Latihan'),
+            Tab(text: 'Profil'),
+          ],
         ),
-        body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(35),
-            child: Column(children: [
-              Center(
-                // this button is used to open the image picker
-                child: ElevatedButton(
-                  onPressed: _openImagePicker,
-                  child: const Text('Select kntl'),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          // Halaman 1: Latihan List
+          Column(
+            children: [
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text('Latihan Dada',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 10),
+                      Text('2 Latihan'),
+                      Text('Incline Bench Press - Dumbbell'),
+                      Text('Incline Bench Press - Barbell'),
+                    ],
+                  ),
                 ),
               ),
-              const SizedBox(height: 35),
-              // The picked image will be displayed here
-              Container(
-                alignment: Alignment.center,
-                width: double.infinity,
-                height: 300,
-                color: const Color.fromARGB(255, 112, 143, 209),
-                child: _image != null
-                    ? Image.file(_image!, fit: BoxFit.cover)
-                    : const Text('Please select mmk'),
-              )
-            ]),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Lihat Semua'),
+                  ),
+                ),
+              ),
+            ],
           ),
-        ));
+
+          // Halaman 2: Profil dan Image Picker
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      // Gambar Profil
+                      GestureDetector(
+                        onTap:
+                            _openImagePicker, // Buka image picker ketika ditekan
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _profileImage != null
+                              ? FileImage(_profileImage!) as ImageProvider
+                              : const AssetImage('assets/default_profile.png'),
+                          child: _profileImage == null
+                              ? const Icon(Icons.camera_alt,
+                                  size: 50, color: Colors.white)
+                              : null,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Selamat Datang, Gym',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: _openImagePicker,
+                        child: const Text('Ubah Foto Profil'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Info Dasar',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Jenis Kelamin'),
+                  trailing: const Text('Laki - Laki'),
+                  onTap: () {}, // Tambahkan fungsionalitas jika diperlukan
+                ),
+                ListTile(
+                  title: const Text('Berat Saat Ini'),
+                  trailing: const Text('74.0 kg'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Target Berat'),
+                  trailing: const Text('70.0 kg'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Tinggi'),
+                  trailing: const Text('171.0 cm'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- flutter_application_prak/lib/app/modules/login/views/login_view.dart
+++ flutter_application_prak/lib/app/modules/login/views/login_view.dart
@@ -1,60 +1,200 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_application_prak/app/modules/home/views/home_view.dart';
-// ignore: depend_on_referenced_packages
-import 'package:flutter_login/flutter_login.dart';
+import 'package:image_picker/image_picker.dart';
 
-const users = {};
+class HomeView extends StatefulWidget {
+  const HomeView({Key? key}) : super(key: key);
 
-class LoginView extends StatelessWidget {
-  const LoginView({super.key});
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
 
-  Duration get loginTime => const Duration(milliseconds: 2250);
+class _HomeViewState extends State<HomeView>
+    with SingleTickerProviderStateMixin {
+  File? _profileImage; // Variabel untuk menyimpan gambar profil
+  final ImagePicker _picker = ImagePicker();
 
-  Future<String?> _authUser(LoginData data) {
-    debugPrint('Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(data.name)) {
-        return 'User not exists';
-      }
-      if (users[data.name] != data.password) {
-        return 'Password does not match';
-      }
-      return Future.value(null);
-    });
+  // Fungsi untuk mengambil gambar dari galeri atau kamera
+  Future<void> _openImagePicker() async {
+    showModalBottomSheet(
+      context: context,
+      builder: (BuildContext context) {
+        return SafeArea(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              ListTile(
+                leading: const Icon(Icons.camera_alt),
+                title: const Text('Ambil Foto dari Kamera'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final XFile? pickedImage =
+                      await _picker.pickImage(source: ImageSource.camera);
+                  if (pickedImage != null) {
+                    setState(() {
+                      _profileImage = File(pickedImage.path);
+                    });
+                  }
+                },
+              ),
+              ListTile(
+                leading: const Icon(Icons.photo),
+                title: const Text('Pilih dari Galeri'),
+                onTap: () async {
+                  Navigator.pop(context);
+                  final XFile? pickedImage =
+                      await _picker.pickImage(source: ImageSource.gallery);
+                  if (pickedImage != null) {
+                    setState(() {
+                      _profileImage = File(pickedImage.path);
+                    });
+                  }
+                },
+              ),
+            ],
+          ),
+        );
+      },
+    );
   }
 
-  Future<String?> _signupUser(SignupData data) {
-    debugPrint('Signup Name: ${data.name}, Password: ${data.password}');
-    return Future.delayed(loginTime).then((_) {
-      return Future.value(null);
-    });
-  }
+  late TabController _tabController;
 
-  Future<String> _recoverPassword(String name) {
-    debugPrint('Name: $name');
-    return Future.delayed(loginTime).then((_) {
-      if (!users.containsKey(name)) {
-        return 'User not exists';
-      }
-      return Future.value(null);
-    });
+  @override
+  void initState() {
+    super.initState();
+    _tabController = TabController(length: 2, vsync: this);
   }
 
   @override
   Widget build(BuildContext context) {
-    return FlutterLogin(
-      logo: const AssetImage('assets/Sign.png'),
-      onLogin: _authUser,
-      onSignup: _signupUser,
-      onSubmitAnimationCompleted: () {
-        Navigator.of(context).pushReplacement(MaterialPageRoute(
-          builder: (context) => const HomeView(),
-        ));
-      },
-      onRecoverPassword: _recoverPassword,
-      theme: LoginTheme(
-        pageColorLight: Colors.lightBlue[100], // Background warna biru muda
-        primaryColor: Colors.lightBlue, // Warna utama
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Latihan Dada'),
+        bottom: TabBar(
+          controller: _tabController,
+          tabs: [
+            Tab(
+              icon: Image.asset(
+                'assets/images/Menu_Latihan.png',
+                height: 24,
+              ),
+              text: 'Latihan',
+            ),
+            Tab(
+              icon: Image.asset(
+                'assets/images/profil.png',
+                height: 24,
+              ),
+              text: 'Profil',
+            ),
+          ],
+        ),
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          // Halaman 1: Latihan List
+          Column(
+            children: [
+              Card(
+                margin: const EdgeInsets.all(16.0),
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: const [
+                      Text('Latihan Dada',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.bold)),
+                      SizedBox(height: 10),
+                      Text('2 Latihan'),
+                      Text('Incline Bench Press - Dumbbell'),
+                      Text('Incline Bench Press - Barbell'),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 16.0),
+                  child: TextButton(
+                    onPressed: () {},
+                    child: const Text('Lihat Semua'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+
+          // Halaman 2: Profil dan Image Picker
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Column(
+                    children: [
+                      // Gambar Profil
+                      GestureDetector(
+                        onTap:
+                            _openImagePicker, // Buka image picker ketika ditekan
+                        child: CircleAvatar(
+                          radius: 50,
+                          backgroundImage: _profileImage != null
+                              ? FileImage(_profileImage!) as ImageProvider
+                              : const AssetImage('assets/default_profile.png'),
+                          child: _profileImage == null
+                              ? const Icon(Icons.camera_alt,
+                                  size: 50, color: Colors.white)
+                              : null,
+                        ),
+                      ),
+                      const SizedBox(height: 10),
+                      const Text(
+                        'Selamat Datang, Gym',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                      TextButton(
+                        onPressed: _openImagePicker,
+                        child: const Text('Ubah Foto Profil'),
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'Info Dasar',
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                ),
+                const Divider(),
+                ListTile(
+                  title: const Text('Jenis Kelamin'),
+                  trailing: const Text('Laki - Laki'),
+                  onTap: () {}, // Tambahkan fungsionalitas jika diperlukan
+                ),
+                ListTile(
+                  title: const Text('Berat Saat Ini'),
+                  trailing: const Text('74.0 kg'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Target Berat'),
+                  trailing: const Text('70.0 kg'),
+                  onTap: () {},
+                ),
+                ListTile(
+                  title: const Text('Tinggi'),
+                  trailing: const Text('171.0 cm'),
+                  onTap: () {},
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
